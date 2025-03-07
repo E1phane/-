37d091fa6aa4e7f2cf5a952e05a54a132d40c189
E1phane <71388065+E1phane@users.noreply.github.com>
2025-03-08 05:17:22 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.21 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public string solution(string s) {
+        string answer = "";
+        if(s.Length % 2 == 0){
+            answer = s.Substring(s.Length / 2 - 1, 2);
+        }
+        else{
+            answer = s.Substring(s.Length / 2, 1);
+        }
+        return answer;
+    }
+}
