be90f4398143f6842743655c1a4a424f2827374e
E1phane <71388065+E1phane@users.noreply.github.com>
2025-03-31 00:43:01 +0900

    [level 1] Title: 명예의 전당 (1), Time: 5.69 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.Length];
+        List<int> honor = new List<int>();
+        
+        for(int i = 0; i < score.Length; i++){
+            if(i < k){
+                honor.Add(score[i]);
+            }
+            else{
+                if(score[i] > honor[0]){
+                    honor[0] = score[i];
+                }
+            }
+            honor.Sort();
+            answer[i] = honor[0];
+        }
+        
+        return answer;
+    }
+}
