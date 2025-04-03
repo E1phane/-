5462b172877870949534446cd43d103c3dbc156f
E1phane <71388065+E1phane@users.noreply.github.com>
2025-04-03 10:12:02 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.31 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cs
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cs
@@ -4,17 +4,13 @@ using System.Collections.Generic;
 public class Solution {
     public int solution(int left, int right) {
         int answer = 0;
-        List<int> result = new List<int>();
         for(int i = left; i <= right; i++){
-            result.Add(i);
-        }
-        foreach(int num in result){
-            double sqrt = Math.Sqrt(num);
+            double sqrt = Math.Sqrt(i);
             if(sqrt == Math.Floor(sqrt)){
-                answer -= num;
+                answer -= i;
             }
             else{
-                answer += num;
+                answer += i;
             }
         }
         return answer;
