b4fa31aa704084711958227564ba5069e356f8ff
E1phane <71388065+E1phane@users.noreply.github.com>
2025-04-07 10:27:41 +0900

    [level 1] Title: 소수 찾기, Time: 10.49 ms, Memory: 31.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        bool[] isPrime = new bool[n+1];
+        
+        for(int i = 2; i <= n; i++){
+            isPrime[i] = true;
+        }
+        
+        for(int i = 2; i * i <= n; i++){
+            if(isPrime[i]){
+                for(int j = i * i; j <= n; j += i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
+        int answer = 0;
+        for(int i = 1; i <= n; i++){
+            if(isPrime[i]){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
