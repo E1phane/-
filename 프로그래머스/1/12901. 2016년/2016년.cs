35f20fae8d4247c1b85e9be24056f96dcd0c6d95
E1phane <71388065+E1phane@users.noreply.github.com>
2025-04-10 16:15:01 +0900

    [level 1] Title: 2016년, Time: 0.24 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cs
+++ 프로그래머스/1/12901. 2016년/2016년.cs
@@ -1,67 +1,17 @@
 public class Solution {
     public string solution(int a, int b) {
         string answer = "";
-        int day = 0;
-        switch(a){
-            case 1:
-                break;
-            case 2:
-                day = 31;
-                break;
-            case 3:
-                day = 31 + 29;
-                break;
-            case 4:
-                day = 31 + 29 + 31;
-                break;
-            case 5:
-                day = 31 + 29 + 31 + 30;
-                break;
-            case 6:
-                day = 31 + 29 + 31 + 30 + 31;
-                break;
-            case 7:
-                day = 31 + 29 + 31 + 30 + 31 + 30;
-                break;
-            case 8:
-                day = 31 + 29 + 31 + 30 + 31 + 30 + 31;
-                break;
-            case 9:
-                day = 31 + 29 + 31 + 30 + 31 + 30 + 31 + 31;
-                break;
-            case 10:
-                day = 31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
-                break;
-            case 11:
-                day = 31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
-                break;
-            case 12:
-                day = 31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
-                break;
+        
+        int[] daysInMonth = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        string[] dayOfWeek = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+
+        int totalDays = b;
+        for(int i = 1; i < a; i++){
+            totalDays += daysInMonth[i];
         }
-        switch((day + b - 1) % 7){
-            case 0:
-                answer = "FRI";
-                break;
-            case 1:
-                answer = "SAT";
-                break;
-            case 2:
-                answer = "SUN";
-                break;
-            case 3:
-                answer = "MON";
-                break;
-            case 4:
-                answer = "TUE";
-                break;
-            case 5:
-                answer = "WED";
-                break;
-            case 6:
-                answer = "THU";
-                break;
-        }
+
+        answer = dayOfWeek[(totalDays - 1) % 7];
         
         return answer;
     }
