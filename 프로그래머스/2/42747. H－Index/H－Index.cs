c512e4173bf11aeac40ad2172a23f76f60b68485
E1phane <71388065+E1phane@users.noreply.github.com>
2025-05-19 00:22:33 +0900

    [level 2] Title: H-Index, Time: 2.62 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int solution(int[] citations) {
+        Array.Sort(citations);
+        int n = citations.Length;
+
+        for (int i = 0; i < n; i++) {
+            int h = n - i;
+            if (citations[i] >= h) {
+                return h;
+            }
+        }
+
+        return 0;
+    }
+}
