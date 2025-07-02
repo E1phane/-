ca19b8faf3d0e2ffc96bba6cfa20847dc337a3fd
E1phane <71388065+E1phane@users.noreply.github.com>
2025-07-03 00:42:06 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 1.51 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int solution(int[] numbers) {
+        Array.Sort(numbers);
+
+        int n = numbers.Length;
+        int max1 = numbers[n - 1] * numbers[n - 2];
+        int max2 = numbers[0] * numbers[1];
+
+        return Math.Max(max1, max2);
+    }
+}
