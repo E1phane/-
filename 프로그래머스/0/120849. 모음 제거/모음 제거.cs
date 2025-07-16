9bf1e8c66766347ed8067762e1778583257fceed
E1phane <71388065+E1phane@users.noreply.github.com>
2025-07-16 19:38:27 +0900

    [level 0] Title: 모음 제거, Time: 0.29 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string) {
+        string answer = "";
+        string vowels = "aeiou";
+        
+        foreach(char c in my_string){
+            if(!vowels.Contains(c))
+                answer += c;
+        }
+        return answer;
+    }
+}
